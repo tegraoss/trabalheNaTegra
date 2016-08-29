c1ab7d732d7d75b156725a2d03893d3beef21c93
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-08-29 19:41:02 -0300

    Add normalize to string

--- testes/word-rank/word-rank.py
+++ testes/word-rank/word-rank.py
@@ -1,12 +1,22 @@
 # Open file in read mode
-TxtFile = open("texto.txt", "r")
+try:
+    TxtFile = open("texto.txt", "r")
+except:
+    print("Não foi possivel abrir o arquivo!")
+    exit()
 # Read data(string) and split into a list of words
-Text = TxtFile.read().split()
-# Sort for better processing
-Text.sort();
+Text = TxtFile.read()
+Text = str(Text)
 
+# Normalize word in lower case and remove
+for Character in [',','.',';',':']:
+    Text = Text.replace(Character, ' ')
+Text = Text.lower()
+# Sort for better processing
+Text = Text.split()
+Text.sort()
 # Set variables
-LastWord =  ""
+LastWord = ""
 WordsList = []
 # Make a list of words and their occurance in tuples
 for Word in Text:
@@ -19,4 +29,8 @@ WordsList.sort(key=lambda word: word[1], reverse=True)
 # Spread the word! At least 20 of them
 for place in range(20):
     Word = WordsList[place]
-    print(str(place + 1) + "st: " + Word[0] + "   Count: " + str(Word[1]))
+    print(Word[0] + " " + str(Word[1]))
+
+# Print entire list
+# for word in WordsList:
+#     print(word[0] + "   Count: " + str(word[1]))
