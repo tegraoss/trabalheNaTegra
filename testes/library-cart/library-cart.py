3edba74a6bdba9c806b1f1e10740aec2abc9c19a
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-09-06 21:13:00 -0300

    Add function to library-cart

--- testes/library-cart/library-cart.py
+++ testes/library-cart/library-cart.py
@@ -5,11 +5,11 @@ from Cart import cart
 def browseBooks(BooksList,responseMethod):
     for book in BooksList:
         if book.qtd > 0:
-            print('Name: ' + book.nome + '\n' + 'Author: '+ book.autor + '\n' + 'Price: R$ ' + str(book.price))
+            print('Name: ' + book.nome + '\nAuthor: '+ book.autor + '\nPrice: R$ ' + str(book.price) + '\nQuantity' + str(book.qtd))
             responseMethod(book)
 
 def SelectBook(book):
-    print('Y - Add the book to cart\nEnter - to continue')
+    print('\nY - Add the book to cart\nEnter - to continue')
     resp = str(input()).lower()
     if resp == 'y' or resp == 'yes':
         print("Quantity: ")
@@ -19,17 +19,53 @@ def SelectBook(book):
         except:
             print('Quantity Error')
         currentCart.addItem(book)
+def isFloat(s):
+    try:
+        float(s)
+        return True
+    except:
+        return False
+def isInt(s):
+    try:
+        int(s)
+        return True
+    except:
+        return False
+
+def readNewBook(atribute):
+    print('Enter the ' + atribute + ' of the book: ')
+    newAtribute = str(input())
+    return newAtribute
+
+def addNewBook():
+    name = readNewBook('name')
+    author = readNewBook('author')
+    quantity = ''
+    while not isInt(quantity):
+        quantity = readNewBook('quantity')
+    price = ''
+    while not isFloat(price):
+        price = readNewBook('price')
+    try:
+        BooksList.append(Book(name,author, float(price), int(quantity)))
+        return True
+    except:
+        return False
 
 if __name__ ==  '__main__':
     currentCart = cart()
     BooksList = readme_conversor()
-    browseBooks(BooksList, SelectBook)
-    print(currentCart.length)
-    while(true):
+    # browseBooks(BooksList, SelectBook)
+    # print(currentCart.length)
+    while(True):
         # Menu
-        print('A - Add new book') #todo
+        print('A - Add new book') 
         print('B - Browse available books')
-        print('C - View your cart') #todo
+        print('C - Cart') #todo
         print('D - Delete a book') #todo
+        print('E - Exit') #toso
+
+        if str(input()).lower() == 'a':
+            addNewBook()
 
     # browseBooks(currentCart.itensList)
